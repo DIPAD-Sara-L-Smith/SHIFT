cdc8dcdde31422e2f7b3aa912d91eb677f8e019b
David Gillespie <david.gillespie@dvsa.gov.uk>
2020-02-10 13:58:39 +0000

    Started outlining function which performs all forecasts.

---
+++ R/fct_regression.R
@@ -0,0 +1,19 @@
+get_forecasts <- function(df, ind_var, dep_var, start, end, forecasts){
+
+}
+
+get_holtwinters <- function(df, ind_var, start, end){
+
+}
+
+get_decomp <- function(df, ind_var, start, end){
+
+}
+
+get_naive <- function(df, ind_var, start, end) {
+
+}
+
+get_linear <- function(df, ind_var, dep_var, start, end){
+
+}
