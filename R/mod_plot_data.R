38067fdcb876ae97876d5a40fbe21d6c80307ed8
David Gillespie <david.gillespie@dvsa.gov.uk>
2020-02-10 09:50:30 +0000

    Fixed dep_var_selector. When rendering UI you also need to use the ns().

--- R/mod_plot_data.R
+++ R/mod_plot_data.R
@@ -13,11 +13,13 @@
 #' @keywords internal
 #' @export
 #' @importFrom shiny NS tagList uiOutput renderUI selectInput
-#' @importFrom dygraphs renderDygraph
-mod_plot_data_ui <- function(id){
+#' @importFrom dygraphs renderDygraph dygraphOutput
+mod_plot_data_ui <- function(id) {
   ns <- NS(id)
   tagList(
+    actionButton(ns("browser_button"), label = "Browser()"),
     uiOutput(ns("dep_var_selector")),
+    dygraphOutput(ns("dep_var_dygraph"))
   )
 }
 
@@ -27,22 +29,33 @@ mod_plot_data_ui <- function(id){
 #' @export
 #' @keywords internal
 
-mod_plot_data_server <- function(input, output, session, r){
+mod_plot_data_server <- function(input, output, session, r) {
   ns <- session$ns
 
   # Selector for dependent variable
   output$dep_var_selector <- renderUI({
     req(r$data)
-    selectInput("dep_var_selector",
-                label = "Select you Dependent Variable:",
-                choices = names(r$data %>% select(-c("Year", "Quarter"))),
-                )
+    items <- names(r$data %>% select(-c("Year", "Quarter")))
+
+    selectInput(ns("dep_var_selector"),
+      label = "Select you Independent Variable:",
+      choices = items,
+      selected = items[1]
+    )
   })
 
-  # dyGraph of the independent variable
-  output$ind_var_dygraph <- renderDygraph({
-    req(r$data, input$ind_var_selector)
-    #function to convert from df to dygraph
+  observeEvent(input$dep_var_selector, {
+    # dyGraph of the independent variable
+    output$dep_var_dygraph <- renderDygraph({
+      req(r$data, input$dep_var_selector)
+      # function to convert from df to dygraph
+      browser()
+      df.ts <- df_to_ts(r$data)
+    })
+  })
+
+  observeEvent(input$browser_button, {
+    browser()
   })
 }
 
@@ -51,4 +64,3 @@ mod_plot_data_server <- function(input, output, session, r){
 
 ## To be copied in the server
 # callModule(mod_plot_data_server, "plot_data_ui_1")
-
