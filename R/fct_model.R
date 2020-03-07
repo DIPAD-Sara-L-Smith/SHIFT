d6c718317ca2b6ee76c34a670b582739b27e548f
saralsmith <jaksbin@gmail.com>
2020-03-07 23:24:25 +0000

    Add predict_models function
    -  Also fix fit_decomp();

--- R/fct_model.R
+++ R/fct_model.R
@@ -1,18 +1,51 @@
-fit_models <- function(xf, dep_var, dep_var, start, end, forecasts){
+
+## Fit / Predict models (all types) -----
+
+fit_models <- function(xf, dep_var, ind_var, start, end, forecasts){
   # Not tested
   fits <- lapply(forecasts, function(){
-  switch(forecast,
-         'holtwinters' = get_holtwinters(),
-         'decomp' = get_decomp(),
-         'naive' = get_naive(),
-         'linear' = get_linear(),
-         # Default case
-         {warning('Forecast type not recognised.')
-           NULL})
-
+    switch(forecast,
+           'holtwinters' = get_holtwinters(),
+           'decomp' = get_decomp(),
+           'naive' = get_naive(),
+           'linear' = get_linear(),
+           # Default case
+           {warning('Forecast type not recognised.')
+             NULL})
   })
 
 }
+
+#' predict_models - calculate forecasts
+#'
+#' @param fit list - model object(s) containing fit
+
+#' @param dep_var string - name of dependent variable to forecast
+#' @param ind_var string - vector of names of independent variables to fit
+#' @param start vector - start year and quarter in format c(YYYY, Q)
+#' @param end vector - end year and quarter in format c(YYYY, Q)
+#'
+#' @return data frame containing predictions/forecasts for multiple models
+#' @importFrom forecast forecast
+#' @export
+predict_models <- function(model_fits, n_periods_to_forecast = 4){
+  # run through all model fits and produce predictions
+  predict_models <- lapply(model_fits, function(fit){
+    # TODO improve this check if fit is valid
+    if (any(attr(fit, "class") %in% c("HoltWinters",
+                                  "forecast",
+                                  "stl",
+                                  "tslm"))) {
+      return(forecast::forecast(fit, h = n_periods_to_forecast))
+    } else {
+      warning("Error in predict_models(): fit object not valid.")
+    }
+  })
+}
+
+
+
+## Holt-Winters -----
 
 #' fit_holtwinters - takes a data frame and returns the fit object for a
 #' Holt-Winters forecast on the data.
@@ -28,13 +61,16 @@ fit_models <- function(xf, dep_var, dep_var, start, end, forecasts){
 #' @importFrom stats HoltWinters
 #' @importFrom stats window
 fit_holtwinters <- function(df, dep_var, start, end){
-  # subset to the selected variable
-  df_xts <- df_to_xts(df, dep_var, start, end)
+  # subset to the selected variablef
+  df_ts <- df_to_ts(df, dep_var, start, end)
 
   # get fit object
-  fit <- stats::HoltWinters(df_xts)
+  fit <- stats::HoltWinters(df_ts)
   return(fit)
 }
+
+
+## Time Series Decomposition -----
 
 #' fit_decomp - takes a data frame and returns the fit object for a time series
 #' decomposition forecast on the data.
@@ -51,12 +87,15 @@ fit_holtwinters <- function(df, dep_var, start, end){
 #' @importFrom stats window
 fit_decomp <- function(df, dep_var, start, end){
   # subset to the selected variable
-  df_xts <- df_to_xts(df, dep_var, start, end)
+  df_ts <- df_to_ts(df, dep_var, start, end)
 
   # get fit object
-  fit <- stats::stl(df_xts, s.window = "period")
+  fit <- stats::stl(df_ts[, dep_var], s.window = "period")
   return(fit)
 }
+
+
+## Naive -----
 
 #' fit_naive - takes a data frame and returns the fit object for a naive
 #' forecast on the data.
@@ -72,14 +111,18 @@ fit_decomp <- function(df, dep_var, start, end){
 #' @importFrom forecast naive
 fit_naive <- function(df, dep_var, start, end) {
   # subset to the selected variable
-  df_xts <- df_to_xts(df, dep_var, start, end)
+  df_ts <- df_to_ts(df, dep_var, start, end)
 
   # get fit object
   fit <- forecast::naive(df)
   return(fit)
 }
 
+
+## Linear Regression -----
+
 # TODO set defaults for start/end throughout this script
+
 #' fit_linear - takes a data frame and returns the fit object for a linear
 #' regression forecast on the data.
 #'
@@ -102,9 +145,4 @@ fit_linear <- function(df, dep_var, ind_var, start, end){
   fit <- forecast::tslm(formula = as.formula(paste0(dep_var, " ~ . + season")),
                         data = df_ts)
   return(fit)
-}
-
-
-predict_models <- function(fits ){
-
 }
