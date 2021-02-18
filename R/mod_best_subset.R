c243a652231f47d93fb14a62b47ecb2d48794022
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-02-18 11:37:18 +0000

    move plot cormat code to fct_cormat finished

--- R/mod_best_subset.R
+++ R/mod_best_subset.R
@@ -527,48 +527,9 @@ mod_best_subset_server <- function(input, output, session, r) {
   # Render Collinearity Matrix
   observeEvent(r$allsubset, {
     req(r$data, input$ind_var_selector)
-    dataplot <- select(r$data, c(input$ind_var_selector))
-    cormat <- round(cor(dataplot),2)
-    melted_cormat <- reshape2::melt(cormat)
-    lower_tri <- cormat
-    lower_tri[lower.tri(lower_tri)] <- NA #OR upper.tri function
-    #Finished correlation matrix heatmap
-    melted_cormat <- reshape2::melt(lower_tri, na.rm = TRUE)
-    # Heatmap
-    my_plot <- ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
-      geom_tile(color = "white")+
-      scale_fill_gradient2(low = "blue",
-                           high = "red",
-                           mid = "white",
-                           midpoint = 0,
-                           limit = c(-1,1),
-                           space = "Lab",
-                           name="Pearson\nCorrelation") +
-      theme_minimal()+
-      theme(axis.text.x = element_text(angle = 45,
-                                       vjust = 1,
-                                       size = 12,
-                                       hjust = 1))+
-      coord_fixed() +
-      geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-      theme(
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.ticks = element_blank(),
-        legend.justification = c(1, 0),
-        legend.position = c(0.6, 0.7),
-        legend.direction = "horizontal")+
-      guides(fill = guide_colorbar(barwidth = 7,
-                                   barheight = 1,
-                                   title.position = "top",
-                                   title.hjust = 0.5))
 
     output$coll_mat <- renderPlot({
       plot_cormat(r$data, input$ind_var_selector)
-      #my_plot
     })
   })
 
