7906abe2fc14b05bab181251593617bdc72d23f1
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2021-03-05 08:38:45 +0000

    add tests for fct_model

--- tests/testthat/test-fct_model.R
+++ tests/testthat/test-fct_model.R
@@ -2,10 +2,10 @@
 #
 # list of functions
 #
-# fit_models
-# predict_models
-# get_forecast_plotdata
-# plot_forecast
+# fit_models - UNUSED?
+# predict_models - DONE
+# TODO get_forecast_plotdata
+# plot_forecast -TEST WITH SHINYTEST
 # fit_holtwinters - DONE
 # fit_decomp - DONE
 # fit_naive - DONE
@@ -14,27 +14,55 @@
 # get_starting_values - UNUSED?
 
 #load sample/test data
-
 test_data <- readr::read_csv("../../example-data/swiss.csv")
 cnames <- names(test_data)
 start <- c(test_data[[1,"Year"]], test_data[[1,"Quarter"]])
 # hotlwinters/naive needs complete years, so take 3 off row
 last_row <- dim(test_data)[1] - 3
 end <- c(test_data[[last_row,"Year"]], test_data[[last_row,"Quarter"]])
+test_ts_lm <- ts(test_data, frequency = 4, start = start)
+test_ts <-  ts(test_data[cnames[1]], frequency = 4, start = start, end = end)
+model <- paste0(cnames[1], " ~ ", paste(cnames[2:4], collapse = " + "))
 
-fc_names = c("method", "model", "lambda", "x", "fitted", "residuals",
-             "series", "mean", "level", "lower", "upper")
+comp_fit_hw <- stats::HoltWinters(test_ts)
+comp_fit_dc <- stats::stl(test_ts, s.window = "period")
+comp_fit_na <- forecast::naive(test_ts)
+comp_fit_lm <- forecast::tslm(formula = as.formula(paste0(model," + trend + season", collapse = "")), data = test_ts_lm)
+
 
 # fit_models
 # looks unused
 # evidence - fit_linear has been updated to require model argument
 # this code does provide that argument
 
-# predict_models
+# predict_models - holtwinters
+test_that("predict_models predicts holtwinters",{
+  test_fc <- predict_models(list(comp_fit_hw))
+  comp_fc <- forecast(comp_fit_hw, h = 4)
+  expect_equal(test_fc[[1]], comp_fc)
+})
+
+# predict_models - decomp
+test_that("predict_models predicts stl / decomp", {
+  test_fc <- predict_models(list(comp_fit_dc))
+  comp_fc <- forecast(comp_fit_dc, h = 4)
+  expect_equal(test_fc[[1]], comp_fc)
+})
+
+# predict_models - naive
 test_that("predict_model predicts naive", {
-  test_fit <- fit_naive(test_data, cnames[1], start, end)
-  test_fc <- predict_models(list(test_fit))
-  expect_equal(names(test_fc[[1]]), fc_names)
+  comp_fc <- forecast(comp_fit_na, h = 4)
+  test_fc <- predict_models(list(comp_fit_na))
+  expect_equal(test_fc[[1]], comp_fc)
+})
+
+# predict_models - linear
+test_that("predict_models predicts linear", {
+  test_proj_data <- test_data[5:8,]
+  comp_fc <- forecast(comp_fit_lm, newdata = test_proj_data)
+  test_fc <- predict_models(list(comp_fit_lm), proj_data = test_proj_data)
+  test_fc <- test_fc[[1]]
+  expect_equal(test_fc$model$coefficients, comp_fc$model$coefficients)
 })
 
 
@@ -54,26 +82,25 @@ test_that("predict_model predicts naive", {
 # needs complete seasons
 test_that("fit_holtwinters produces HoltWinters object",{
   test_fit <- fit_holtwinters(test_data, cnames[1], start, end)
-  expect_equal(class(test_fit), "HoltWinters")
+  expect_equal(test_fit$coefficients, comp_fit_hw$coefficients)
 })
 
 
 # fit_decomp
 test_that("fit_decomp produces stl object", {
   test_fit <- fit_decomp(test_data, cnames[1])
-  expect_equal(class(test_fit), "stl")
+  expect_equal(test_fit$coefficients, comp_fit_dc$coefficients)
 })
 
 
 # fit_naive
 test_that("fit_naive produces forecast object", {
   test_fit <- fit_naive(test_data, cnames[1], start, end)
-  expect_equal(class(test_fit), "forecast")
+  expect_equal(test_fit$fitted, comp_fit_na$fitted)
 })
 
 # fit_linear
 test_that("fit_liner produces tslm / lm object", {
-  model <- paste0(cnames[1], " ~ ", paste(cnames[2:4], collapse = " + "))
   test_fit <- fit_linear(test_data, cnames[1], cnames[2:4], start, end, model)
   expect_equal(class(test_fit),c("tslm", "lm"))
 })
